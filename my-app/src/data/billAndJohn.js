3330077d03b35f1f3b0c6cb06e71494ab2c540fb
Trevor Baker <trevor-baker@live.com.au>
2026-04-04 17:26:54 +1000

    TASK-06: Created dialogue for Bill and John

---
+++ my-app/src/data/billAndJohn.js
@@ -0,0 +1,12 @@
+const billAndJohn = 
+[
+    {name: "John", words: "Good Monday Morning, Bill! It's John James! I believe we met at the last software convention in Shoreside?"},
+    {name: "Bill", words: "Umm... Hello John? I am having a hard time recalling you. I met a lot of people at the last convention. Could you remind me which company you work for?"},
+    {name: "John", words: "I work for R.E.V.S Industries! We create cheap and reliable anti-software technology! I believe your company... "},
+    {name: "Bill", words: "Sorry John, I think I am going to have to stop you there... I remember you now. Unfortunately, we really need to keep spending tight at the moment."},
+    {name: "Bill", words: "While I am the head of a major software company, my expertise are in business, and I do not have the time to be looking into random anti-virus software at the moment."},
+    {name: "John", words: "I promise you that I am no expert either. I'm not here to bore you with technical details. Our software will keep you safe and secure."},
+    {name: "Bill", words: "You seem sure of your anti-virus, but I really need to go now..."},
+    {name: "John", words: "What would your co-workers think if they knew you think they're expendable?"},
+    {name: "Bill", words: "You were eavesdropping on me! I knew someone was listening... Fine. Lets make a deal..."},
+]; 
