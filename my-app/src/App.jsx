9358d543d658bddba46397ca03df4f31abfbc189
Trevor Baker <trevor-baker@live.com.au>
2026-04-04 20:45:40 +1000

    TASK-07: Make modifications to the flow of the game by making the gameplay linear

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,15 +12,16 @@ function App() {
 
   const [magaretSolved, setMargaretSolved] = useState(null); 
   const [billSolved, setBillSolved] = useState(null); 
+  const [johnSolved, setJohnSolved] = useState(null); 
 
  return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Start/>}/>
-      <Route path="/home" element={<Home margaretSolved={magaretSolved} billSolved={billSolved}/>}/>
+      <Route path="/home" element={<Home margaretSolved={magaretSolved} billSolved={billSolved} johnSolved={johnSolved}/>}/>
       <Route path="/margaret" element={<Margarete setSolved={setMargaretSolved} solved={magaretSolved}/>}/>
       <Route path="/bill" element={<Bill setSolved={setBillSolved} solved={billSolved}/>}/> 
-      <Route path="/john" element={<John/>}/>
+      <Route path="/john" element={<John setSolved={setJohnSolved} solved={johnSolved}/>}/>
       <Route path="/theChoice" element={<TheChoice/>}/>
     </Routes>
   </BrowserRouter>
