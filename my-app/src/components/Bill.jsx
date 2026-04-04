b85462b728fa7ec1cbcbda822931134d5e4929f0
Trevor Baker <trevor-baker@live.com.au>
2026-04-04 17:40:22 +1000

    TASK-06: Added the Bill and John Dialogue to the Bill page

--- my-app/src/components/Bill.jsx
+++ my-app/src/components/Bill.jsx
@@ -1,14 +1,26 @@
 import Navigate from './Navigate';
 import { useNavigate } from 'react-router-dom';
+import ProcessDialogue from './ProcessDialogue';
+import billAndJohnDialogue from '../data/billAndJohnDialogue'
+
+
 
 function Bill()
 {
   const nav = useNavigate();
   const {handleHome} = Navigate(nav); 
+  
+  const dialogue = ProcessDialogue(billAndJohnDialogue); 
 
  return (
       <>
-        <h1 id="Title">Bill</h1>
+        <h1 id="Title">Bill Challenge</h1>
+               
+
+        <div id="LeftBoxStyle">
+            {dialogue}
+        </div>
+
         <button onClick={handleHome}>Return to home</button>
       </>
  );
