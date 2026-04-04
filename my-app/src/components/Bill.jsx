3116aef41e631ef3549b9296fc5eca3282722f35
Trevor Baker <trevor-baker@live.com.au>
2026-04-04 19:28:59 +1000

    TASK-06: Crafted the riddle for Bills Challenge

--- my-app/src/components/Bill.jsx
+++ my-app/src/components/Bill.jsx
@@ -4,7 +4,6 @@ import ProcessDialogue from './ProcessDialogue';
 import billAndJohnDialogue from '../data/billAndJohnDialogue'
 
 
-
 function Bill()
 {
   const nav = useNavigate();
@@ -15,8 +14,15 @@ function Bill()
  return (
       <>
         <h1 id="Title">Bill Challenge</h1>
-               
 
+        <div id="CenterBoxStyle">
+          <p>Solve the following riddle using your phone call with Bill for clues.</p>
+          <p><i>My rectangular interior is dark and cold</i></p>
+          <p><i>Secrets of yours, I will hold</i></p>
+          <p><i>A simple code protects all</i></p>
+          <p><i>What am I?</i></p>
+        </div>
+               
         <div id="LeftBoxStyle">
             {dialogue}
         </div>
