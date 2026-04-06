170f65be367302c6802a953b4b1fda3d9c1804d0
Trevor Baker <trevor-baker@live.com.au>
2026-04-06 11:55:58 +1000

    TASK-09: Improved the functionality of making the option elements and assigning the values correctly

--- my-app/src/components/John.jsx
+++ my-app/src/components/John.jsx
@@ -7,24 +7,75 @@ function John({setSolved, solved})
   const nav = useNavigate();
   const {handleHome} = Navigate(nav); 
 
-  const [option1, setOption1] = useState(""); 
-  const [option2, setOption2] = useState(""); 
-  const [option3, setOption3] = useState(""); 
-  const [option4, setOption4] = useState(""); 
-  const [option5, setOption5] = useState(""); 
+  // const [option1, setOption1] = useState(""); 
+  // const [option2, setOption2] = useState(""); 
+  // const [option3, setOption3] = useState(""); 
+  // const [option4, setOption4] = useState(""); 
+  // const [option5, setOption5] = useState(""); 
 
-  const optionList1 = ["John", "Bill", "Margarete"];
-  const optionList2 = ["$6.75", "4 months", "$27"];
-  const optionList3 = ["assistant manager", "boss", "op shop", "software company"];
-  const optionList4 = ["Eavesdropping", "Expert", "Expendable"];
-  const optionList5 = ["safe", "secure", "software", "special", "Shoreside"];
+  // const optionList1 = ["John", "Bill", "Margarete"];
+  // const optionList2 = ["$6.75", "4 months", "$27"];
+  // const optionList3 = ["assistant manager", "boss", "op shop", "software company"];
+  // const optionList4 = ["Eavesdropping", "Expert", "Expendable"];
+  // const optionList5 = ["safe", "secure", "software", "special", "Shoreside"];
+
+  const optionLists = [
+    ["John", "Bill", "Margarete"],
+    ["$6.75", "4 months", "$27"],
+    ["assistant manager", "boss", "op shop", "software company"],
+    ["Eavesdropping", "Expert", "Expendable"],
+    ["safe", "secure", "software", "special", "Shoreside"]
+  ]
+
+  const [selectedOptions, setSelectedOptions] = useState(new Array(5).fill(null));
+
+  const answerList = new Array(5).fill({hidden: true, answer: ""}); 
+  const roundCounter = 0; 
+  const timeoutValue = 1000;
 
   const makeOptionList = (arr) => {
     return arr.map((o) => <option id={useId()}>{o}</option>);
   }
 
-  const SelectOption = (fn, e) => fn(e.target.value);
+  const SelectOption = (i, e) => {
+    //Deep copy the array. 
+    //This is because useState is immutable, and a shallow copy will modify the original elements. 
+    const newArray = structuredClone(selectedOptions);
+    newArray[i] = e.target.value; 
+    console.log(newArray); 
+    setSelectedOptions(arr => arr = newArray);
+  }
 
+  const startButtonPressed = () => { }
+
+
+
+  function CreateDropdownContainers()
+  {
+    const elements = [];
+
+    //Use let instead of var here, as var i is function-scoped instead of block scoped. 
+    //We do not need SelectOption(i,e) containing the same i value for each onChange
+    for (let i = 0; i < optionLists.length; i++)
+    { 
+       elements.push(
+        <div id="DropdownContainer">
+           <p hidden={answerList[i].hidden}>{answerList[i].answer}</p>
+           <p>Option {i+1}</p>
+           <select defaultValue="0" onChange={(e) => SelectOption(i, e)}>
+            <option value="0" disabled id={useId()}>Pick an option</option>
+              {makeOptionList(optionLists[i])}
+            </select>
+        </div>
+       )
+    }
+
+    return (
+      <div id="SingleRowStyle">
+         {elements}
+      </div>
+    );
+  }
 
   
  return (
@@ -36,33 +87,9 @@ function John({setSolved, solved})
             <p>Let's see if you can finish my pattern matching quiz.</p>
             <p>You need to match the words in order of their appearance...</p>
 
-            <div id="SingleRowStyle">
-              <div id="DropdownContainer">
-                <p>Option 1</p>
-                <select value={option1} onChange={(e) => SelectOption(setOption1, e)}>{makeOptionList(optionList1)}</select>
-              </div>
+            <button onClick={startButtonPressed}>Start</button>
 
-              <div id="DropdownContainer">
-                <p>Option 2</p>
-                <select value={option2} onChange={(e) => SelectOption(setOption2, e)}>{makeOptionList(optionList2)}</select>
-              </div>
-
-              <div id="DropdownContainer">
-                <p>Option 3</p>
-                <select value={option3} onChange={(e) => SelectOption(setOption3, e)}>{makeOptionList(optionList3)}</select>
-              </div>
-
-              <div id="DropdownContainer">
-                <p>Option 4</p>
-                <select value={option4} onChange={(e) => SelectOption(setOption4, e)}>{makeOptionList(optionList4)}</select>
-              </div>
-
-              <div id="DropdownContainer">
-                <p>Option 5</p>
-                <select value={option5} onChange={(e) => SelectOption(setOption5, e)}>{makeOptionList(optionList5)}</select>
-              </div>
-
-            </div>
+            {CreateDropdownContainers()}
         </div>
 
         
